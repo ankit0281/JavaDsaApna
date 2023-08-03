8458aef1c85a3fa6d96046b0d19a1d6280182aa6
ankit0281 <94033670+ankit0281@users.noreply.github.com>
2023-08-03 10:54:33 +0530

    Recursion

---
+++ SumOfNaturalNos.java
@@ -0,0 +1,23 @@
+public class SumOfNaturalNos {
+    public static void printDec(int n) {
+        if(n == 1){
+            System.out.println(n);
+            return;
+
+        }
+    }
+    
+    public static int calcSum(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int Snm1 = calcSum(n-1);
+        int Sn = n + Snm1;
+        return Sn;
+    }
+    public static void main(String args[]) {
+        int n = 10;
+        System.out.println(calcSum(n));
+    }
+}
+
